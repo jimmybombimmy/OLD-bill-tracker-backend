916295deb5e21d9d5d70cbaa35fa48615c0076c3
jimmybombimmy <jamespeters92@gmail.com>
2023-11-02 16:56:20 +0000

    add test for time in BST

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,11 +1,20 @@
 const {convertTimestampToDate} = require('../dist/db/seed-files/utils.js')
 
 describe("convertTimestampToDate", () => {
-    test('check that result comes back as a date object', () => {
+    test('result comes back as a date object', () => {
         const timestamp = 1557572706232;
-        const result = convertTimestampToDate(timestamp)
-        expect(isNaN(result)).toBe(false)
-        expect(typeof result).toBe('object')
+        const convert = convertTimestampToDate(timestamp)
+        expect(isNaN(convert)).toBe(false)
+        expect(typeof convert).toBe('object')
+    })
+
+    //Warning: This test will fail if clock not set to British Summer Time
+    test.skip('function is the correct time', () => {
+        const timestamp = 1557572706232;
+        const convert = convertTimestampToDate(timestamp)
+        const result = convert.toString()
+        expect(result).toBe('Sat May 11 2019 12:05:06 GMT+0100 (British Summer Time)')
+        
     })
 })
 
