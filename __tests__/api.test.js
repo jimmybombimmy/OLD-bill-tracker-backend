ebdf4f93fa1a850caac1a8c4a5921ada4b2cae17
jimmybombimmy <jamespeters92@gmail.com>
2023-11-03 13:06:25 +0000

    minor fix to help test seed db

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,18 +1,42 @@
 const app = require('../dist/app.js');
 const request = require('supertest');
 
-const seed = require('../dist/db/seed-files/seed.js')
+const {seed} = require('../dist/db/seed-files/seed.js')
 const testData = require('../dist/data/test-data')
-const connection = require('../dist/db/connection.js')
+const {db} = require('../dist/db/connection.js')
+const connection = db
 
 beforeEach(() => seed(testData))
 afterAll(() => connection.end());
 
+
+////////Test Template////////
 // describe('[REQUEST] [ENDPOINT]' , () => {
 //   describe('Successful connection tests(s)', () => {
-//     test('[STATUS CODE]: [DESCRIPTION] ')
+//     test('[STATUS CODE]: [DESCRIPTION]', () => {
+
+// })
 //   })
 //   describe('Unsuccessful connection tests(s)', () => {
 
 //   })
 // })
+
+describe('GET /api/users' , () => {
+  describe('Successful connection tests(s)', () => {
+    test('200: page returns an array of objects ', () => {
+      return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({
+          body
+        }) => {
+          expect(body.isArray()).toBe(true)
+        })
+
+    })
+  })
+  describe('Unsuccessful connection tests(s)', () => {
+
+  })
+})
